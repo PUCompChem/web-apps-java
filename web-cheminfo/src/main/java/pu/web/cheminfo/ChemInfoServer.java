cb67f2d73afb7ea4bf017c58d27c46eaca5dde06
ntk73 <nick@uni-plovdiv.net>
2022-04-29 12:11:50 +0300

    Added new class ChemInforServer

---
+++ web-cheminfo/src/main/java/pu/web/cheminfo/ChemInfoServer.java
@@ -0,0 +1,26 @@
+package pu.web.cheminfo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openscience.cdk.interfaces.IAtomContainer;
+
+public class ChemInfoServer 
+{
+	public static ChemInfoServer defaultServer = new ChemInfoServer();
+	
+	//Config data
+	public int maxNumberOfMolecules = 200;
+
+	//Work data
+	public List<IAtomContainer> molecules = new ArrayList<IAtomContainer>();
+	public int nextID = 1;
+	
+	public void addMolecule(IAtomContainer mol) {
+		if (molecules.size() >= maxNumberOfMolecules)
+			molecules.remove(0);
+		molecules.add(mol);
+		mol.setProperty("M_ID", nextID);
+		nextID++;
+	}
+}
